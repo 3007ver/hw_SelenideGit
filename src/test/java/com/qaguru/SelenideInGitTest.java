32073d9628e6339b2cfb5b5ad55fad4d6030582f
3007ver <veronika.shyshlo@gmail.com>
2023-04-15 23:14:06 +0300

    mistakes were corrected

--- src/test/java/com/qaguru/SelenideInGitTest.java
+++ src/test/java/com/qaguru/SelenideInGitTest.java
@@ -9,14 +9,14 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideInGitTest extends TestBase {
     @Test
-    void SoftAssertionsSearchTests () {
+    void softAssertionsSearchTests () {
         open("https://github.com");
         $("[placeholder = 'Search GitHub']").setValue("selenide").pressEnter();
         $$("ul.repo-list li").first().$("a").click();
         $("#wiki-tab").click();
-        //$("#wiki-pages-box").$(byText("Show 2 more pages...")).click();
-        $("#wiki-body").shouldHave(text("Soft assertions"));
-        $("#wiki-body").$(byText("Soft assertions")).click();
+        $("button.js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
 
 
